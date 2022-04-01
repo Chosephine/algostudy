32ebaf1f4acdaacbabfdc5f5e0610522b5acfc0b
Eunice <backji5@gmail.com>
2022-04-01 23:50:55 +0900

    add basic boj10172

---
+++ basic/boj10172.py
@@ -0,0 +1,10 @@
+# boj10172 개
+# https://www.acmicpc.net/problem/10172
+# \는 escape sequence로, string에서 \를 그대로 사용하기 위해서는 \\를 사용해야 한다!
+
+
+print('|\\_/|')
+print('|q p|   /}')
+print('( 0 )"""\\')
+print('|"^"`    |')
+print('||_/=\\\\__|')
