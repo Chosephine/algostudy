ca3e64c3d438472e57f42028a3fb94503371991a
Chosephine <backji5@gmail.com>
2022-04-07 23:13:38 +0900

    add basic boj10818

---
+++ basic/boj10818.py
@@ -0,0 +1,10 @@
+# boj10818 최소, 최대
+# https://www.acmicpc.net/problem/10818
+
+import sys
+input = sys.stdin.readline
+
+N = int(input())
+nums = list(map(int, input().split()))
+
+print(min(nums), max(nums))
