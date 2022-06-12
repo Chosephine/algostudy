32bef128572af59d39f8490d03f45bb1254f6a65
Chosephine <backji5@gmail.com>
2022-06-13 01:21:15 +0900

    add math programmers12985

---
+++ math/programmers12985.py
@@ -0,0 +1,11 @@
+# 프로그래머스 2017 팁스타운 예상대진표
+# https://programmers.co.kr/learn/courses/30/lessons/12985
+
+def solution(n,a,b):
+    answer = 0
+    while a != b:
+        a = (a + 1)//2
+        b = (b + 1)//2
+        answer += 1
+
+    return answer
