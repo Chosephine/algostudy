eb14506f7db50eb77f67cc0df045df60087f1211
Chosephine <backji5@gmail.com>
2022-08-02 21:21:00 +0900

    modify greedy pfc_greedy_05

--- greedy/pfc_greedy_05.py
+++ greedy/pfc_greedy_05.py
@@ -2,11 +2,16 @@ N, M = map(int, input().split())
 arr = list(map(int, input().split()))
 
 num_cnt = [0] * (M + 1)
-over2 = 0
+
 for a in arr:
     num_cnt[a] += 1
-    if num_cnt[a] == 2:
-        over2 += 1
+
+over2 = 0
+for i in range(len(num_cnt)):
+    n = num_cnt[i]
+    if n >= 2:
+        over2 += ((n * (n - 1)) / 2)
+
 
 result = ((N * (N - 1)) / 2) - over2
 
