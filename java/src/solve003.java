86e98a849aae89e64000a99631bfc1bd9d0637ab
Chosephine <backji5@gmail.com>
2023-02-21 00:56:39 +0900

    add java programmers 분수의덧셈

---
+++ java/src/solve003.java
@@ -0,0 +1,41 @@
+/*
+ * programmers 코딩테스트 입문
+ * 분수의 덧셈
+*/
+
+public class solve003{
+	// 유클리드 호제법을 활용한 최대공약수 찾기 (n1 > n2)
+	public static int gdc(int n1, int n2) {
+		int a = n1;
+		int b = n2;
+		int r = a % b;
+		while (r != 0) {
+			a = b;
+			b = r;
+			r = a % b;
+		}
+		return b;
+	}
+	
+    public static int[] solution(int numer1, int denom1, int numer2, int denom2) {
+        
+        
+        // 덧셈 진행
+        int denom = denom1 * denom2;
+        int numer = numer1 * denom2 + numer2 * denom1;
+        
+        // 분자, 분모의 최대공약수 찾기
+        int gdc = gdc(Math.max(numer, denom), Math.min(numer,  denom));
+        
+        // 정답 작성
+        int[] answer = new int[]{numer / gdc, denom / gdc};
+        
+        return answer;
+    }
+    
+    public static void main(String[] args) throws Exception {
+        
+    	System.out.print(solution(1, 2, 3, 4));
+    	
+    }
+}
