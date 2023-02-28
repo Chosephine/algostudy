785a5ef550a95405a9e3eee70ecb132de015e232
Chosephine <backji5@gmail.com>
2023-02-28 23:57:04 +0900

    add java programmers 나이 출력

---
+++ java/src/solve014.java
@@ -0,0 +1,24 @@
+
+
+/*
+ * programmers 코딩테스트 입문
+ * 나이 출력
+*/
+
+public class solve014{
+	
+	public static int solution(int age) {
+//        int answer = 2023 - age;
+        return 2023 - age;
+    }
+
+    public static void main(String[] args) throws Exception {
+    	System.out.printf("hello world\n");
+    	int inPut = 23;
+//    	int people = 12;
+    	int answer = solution(inPut);
+//    	ArrayList<Integer> answer = solution(inPut);
+    	System.out.println(answer);
+//    	System.out.println(answer);
+    }
+}
