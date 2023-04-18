82b816efec4f7794c22915a37a5be7f54155e4bd
Eunice <backji5@gmail.com>
2023-04-18 23:19:20 +0900

    add java programmers 주사위의 개수

--- java/src/src/solve034.java
+++ java/src/src/solve034.java
@@ -1,13 +1,17 @@
 package src;
 import java.util.Arrays;
 
+/**
+ * 배열 회전시키기
+ * */
+
 public class solve034 {
 	
 	public static int[] solution(int[] numbers, String direction) {
 		int n = numbers.length;
         int[] answer = new int[n];
         if (direction.equals("right")) {
-        	// 0 1 2 => 2 0 1
+        	// 참조형 객체를 내부를 비교하기 위해서는 equals 사용!
         	answer[0] = numbers[n-1];
         	for (int i=0; i < n-1; i++) {
         		answer[i + 1] = numbers[i];
