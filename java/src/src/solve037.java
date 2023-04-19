daaff24f2bf15e29bb814ce255e57e90faa17228
Eunice <backji5@gmail.com>
2023-04-19 23:11:42 +0900

    add java programmers 최댓값 만들기(1)

---
+++ java/src/src/solve037.java
@@ -0,0 +1,23 @@
+package src;
+import java.util.Arrays;
+
+/*
+ * 최댓값 만들기(1) 
+*/
+
+// 직접 소팅을 구현해야 할까 고민했지만... sort 메소드 사용 ^.^
+
+public class solve037 {
+	public static int solution(int[] numbers) {
+        Arrays.sort(numbers);
+        int answer = numbers[numbers.length - 1] * numbers[numbers.length - 2];
+        return answer;
+    }
+	
+	public static void main(String[] args) {
+		int[] numbers = {0, 31, 24, 10, 1, 9};
+		int answer = solution(numbers);
+		System.out.println(answer);
+	}
+
+}
