54b368a73eb9ffb643a4b8890ec2ef1da4a5eb7a
Eunice <backji5@gmail.com>
2023-04-25 22:49:53 +0900

    add java programmers 배열 원소의 길

---
+++ java/src/src/solve044.java
@@ -0,0 +1,23 @@
+package src;
+import java.util.Arrays;
+
+/*
+ * 프로그래머스 120854 배열 원소의 길
+*/
+
+public class solve044 {
+	public static int[] solution(String[] strlist) {
+        int[] answer = new int[strlist.length];
+        for (int i=0; i < strlist.length; i++) {
+        	answer[i] = strlist[i].length();
+        }
+        return answer;
+    }
+	public static void main(String[] args) {
+		// String[] 할당하면서 초기화할 때 {} 활용하
+		String[] strlist = {"I", "Love", "Programmers."};
+		int[] answer = solution(strlist);
+		System.out.println(Arrays.toString(answer));
+		
+	}
+}
