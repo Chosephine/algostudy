4478d7c189d8c0084cce942fcd193f180a1fe917
Eunice <backji5@gmail.com>
2023-04-28 00:00:02 +0900

    add java programmers 암호 해독

---
+++ java/src/src/solve049.java
@@ -0,0 +1,24 @@
+package src;
+
+/*
+ * 프로그래머스 120892 암호 해독 
+*/
+
+public class solve049 {
+	public static String solution(String cipher, int code) {
+		char[] charCipher = cipher.toCharArray();
+        String answer = "";
+        for (int i=0; i < cipher.length(); i++) {
+        	if ( (i+1)%code == 0) {
+        		answer += charCipher[i];
+        	}
+        }
+        return answer;
+    }
+	public static void main(String[] args) {
+		String cipher = "pfqallllabwaoclk";
+		int code = 2;
+		String answer = solution(cipher, code);
+		System.out.println(answer);
+	}
+}
