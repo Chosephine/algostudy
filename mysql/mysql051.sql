c8f782affe4fd0620e19d6d3ef25945173006769
Eunice <backji5@gmail.com>
2023-08-01 23:41:28 +0900

    add mysql programmers 즐겨찾기가 가장 많은 식당 정보 출력하기

---
+++ mysql/mysql051.sql
@@ -0,0 +1,12 @@
+-- 프로그래머스 131123 즐겨찾기가 가장 많은 식당 정보 출력하기
+SELECT FOOD_TYPE, REST_ID, REST_NAME, FAVORITES
+FROM REST_INFO
+WHERE (FOOD_TYPE, FAVORITES) IN (
+  SELECT FOOD_TYPE, MAX(FAVORITES)
+  FROM REST_INFO
+  GROUP BY FOOD_TYPE
+)
+ORDER BY FOOD_TYPE DESC;
+
+ -- group by 할 경우 임의의 row를 출력
+ -- having MAX(FAVORITES)을 써도 각 group 별 최상단에 있는 row 출력
