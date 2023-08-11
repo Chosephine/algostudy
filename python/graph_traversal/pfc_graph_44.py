2b1b3da21da94415f8ff926dd42da32ecf459b29
Chosephine <backji5@gmail.com>
2023-08-12 00:47:25 +0900

    modify python pfc 행성 터널

--- python/graph_traversal/pfc_graph_44.py
+++ python/graph_traversal/pfc_graph_44.py
@@ -2,14 +2,17 @@
 # https://www.acmicpc.net/problem/2887
 
 import sys
+import heapq
 
 input = sys.stdin.readline
+
 
 def find_parent(node, relations):
     if relations[node] == node:
         return node
     else:
         return find_parent(relations[node], relations)
+
 
 def union_parent(node1, node2, relations):
     parent1 = relations[node1]
@@ -19,6 +22,7 @@ def union_parent(node1, node2, relations):
     else:
         relations[node1] = parent2
 
+
 # 행성 개수
 N = int(input().strip())
 
@@ -26,22 +30,29 @@ position = []
 roots = [_ for _ in range(N)]
 distances = []
 
-for _ in range(N):
+for n in range(N):
     x, y, z = map(int, input().strip().split())
-    position.append((x, y, z))
+    # n번 행성의 좌표는 X, Y, Z
+    position.append((n, x, y, z))
 
-for r in range(N):
-    sx, sy, sz = position[r]
-    for c in range(r+1, N):
-        ex, ey, ez = position[c]
-        distances.append((min(abs(sx-ex), abs(sy-ey), abs(sz-ez)), r, c))
+for c in range(3):
+    position.sort(key=lambda p: p[c+1])
+    # print(position)
+    for i in range(N-1):
+        n1, x1, y1, z1 = position[i]
+        n2, x2, y2, z2 = position[i+1]
+        if c == 0:
+            heapq.heappush(distances, (abs(x2 - x1), n1, n2))
+        elif c == 1:
+            heapq.heappush(distances, (abs(y2 - y1), n1, n2))
+        else:
+            heapq.heappush(distances, (abs(z2 - z1), n1, n2))
 
-distances.sort()
 
 roads = cost = 0
 
-for i in range(len(distances)):
-    distance, planet1, planet2 = distances[i]
+while roads < N-1:
+    distance, planet1, planet2 = heapq.heappop(distances)
     p1root = find_parent(planet1, roots)
     p2root = find_parent(planet2, roots)
     if p1root == p2root:
@@ -50,7 +61,6 @@ for i in range(len(distances)):
         union_parent(planet1, planet2, roots)
         roads += 1
         cost += distance
-        if roads == N-1:
-            break
+
 
 print(cost)
