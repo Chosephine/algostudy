5adb3f5ca14d3559aaa91aeebaf1f6cffa65e8f4
Eunice <backji5@gmail.com>
2023-09-06 23:21:25 +0900

    add java programmers 가장 큰 수 찾기

---
+++ java/src/src/solve054.java
@@ -0,0 +1,24 @@
+package src;
+
+import java.util.Arrays;
+
+public class solve054 {
+	public static int[] solution(int[] array) {
+        int maxElement = 0;
+        int maxIndex = 0;
+        for (int i=0; i < array.length; i++) {
+        	if (array[i] > maxElement) {
+        		maxElement = array[i];
+        		maxIndex = i;
+        	}
+        }
+        int[] answer = {maxElement, maxIndex};
+        
+        return answer;
+    }
+	public static void main(String[] args) {
+		int[] inputArray = {1, 8, 3};
+		int[] answer = solution(inputArray);
+		System.out.println(Arrays.toString(answer));
+	}
+}
