bab2e63a70f213d6fcc5d2239a00f65075100cdc
Eunice <backji5@gmail.com>
2023-09-09 23:58:46 +0900

    add mysql hackerrank Revising the Select Query I

---
+++ mysql/mysql075.sql
@@ -0,0 +1,6 @@
+-- 해커랭크 Revising the Select Query I (Easy)
+-- https://www.hackerrank.com/challenges/revising-the-select-query/problem?isFullScreen=true
+
+SELECT *
+FROM CITY
+WHERE COUNTRYCODE='USA' AND POPULATION > 100000;
