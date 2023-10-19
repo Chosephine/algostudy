34ac3c9d966794e15316b86cef6cfd54de110917
Eunice <backji5@gmail.com>
2023-10-19 23:53:31 +0900

    modify python programmers 완주하지 못한 선수

--- python/data_structure/programmers42576.py
+++ python/data_structure/programmers42576.py
@@ -14,6 +14,7 @@ def solution1(participant, completion):
 
     return answer
 
+
 # 정확성 58.3, 효율성 0.0
 def solution2(participant, completion):
 
@@ -35,3 +36,24 @@ def solution2(participant, completion):
             return k
 
     return keys
+
+
+# 정확성 58.3, 효율성 41.7
+def solution3(participant, completion):
+
+    dict_participant = dict()
+    hash_sum = 0
+
+    # hash() : 객체의 해시값을 반환 (해시가 있는 경우). 해시값은 정수.
+    # 딕셔너리 조회 중에 딕셔너리 키를 빨리 비교하는 데 사용
+    # 같다고 비교되는 숫자 값은 같은 해시값을 갖습니다 (1과 1.0의 경우와 같이 형이 다른 경우조차도 그러함).
+
+    for player in participant:
+        hashed_player = hash(player)
+        dict_participant[hashed_player] += player
+        hash_sum += hashed_player
+
+    for player in completion:
+        hash_sum -= hash(player)
+
+    return dict_participant[hash_sum]
