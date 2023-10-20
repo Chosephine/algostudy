b3395a006328c87878ca17cbcdc4b81ed9999b08
Eunice <backji5@gmail.com>
2023-10-20 23:03:42 +0900

    add python programmers 전화번호 목록

---
+++ python/data_structure/programmers42577.py
@@ -0,0 +1,20 @@
+# 프로그래머스 42577 전화번호 목록
+# https://school.programmers.co.kr/learn/courses/30/lessons/42577
+
+
+# 정확성 83.3 / 효율성 0.0
+def solution1(phone_book):
+    # 한 번호가 다른 번호의 접두어인 경우가 있으면 False, 없으면 True
+    phone_book.sort(key=lambda x: len(x))
+
+    answer = True
+
+    page = len(phone_book)
+    for i in range(page):
+        l = len(phone_book[i])
+        for j in range(i + 1, page):
+            if phone_book[j][:l] == phone_book[i]:
+                answer = False
+                break
+
+    return answer
