fe706e1064664dd485aed548fdf01f99ea3a5d49
Chosephine <backji5@gmail.com>
2023-11-28 23:09:15 +0900

    modify : python boj 트리의 부모 찾기
    - Time: 476ms, Memory: 53288KB
    - 유니온파인드로 풀리지 않아서 BFS 시도
    - 연결리스트는 메모리초과 발생하여 이어진 노드만 저장하는 방식으로 변경해서 해결

--- python/graph_traversal/boj11725.py
+++ python/graph_traversal/boj11725.py
@@ -1,40 +1,50 @@
 # 백준 11725 트리의 부모 찾기
 # https://www.acmicpc.net/problem/11725
 
-def find_parent(n: int, parent: list):
-    s = n
-    while parent[s] != s:
-        s = parent[s]
-    return s
+import sys
+from collections import deque
+input = sys.stdin.readline
 
+# N : 노드의 개수
+# relations : 각 노드에 연결된 노드들을 담을 리스트
+N = int(input())
+relations = [[] for _ in range(N + 1)]
 
-def union_parent(u: int, v: int, parent: list):
-    u_parent = find_parent(u, parent)
-    v_parent = find_parent(v, parent)
+# 연결된 노드들 정리
+for _ in range(N-1):
+    u, v = map(int, input().split())
+    relations[u].append(v)
+    relations[v].append(u)
 
-    if u == u_parent and v == v_parent:
-        bigger = max(u, v)
-        smaller = min(u, v)
-        parent[bigger] = smaller
-    elif u == u_parent and v != v_parent:
-        parent[u] = v
-    elif v == v_parent and u != u_parent:
-        parent[v] = u
+# parent : 각 노드의 부모를 담을 리스트
+# min_degree : 각 노드에서의 최소 차수. 최소 차수인 노드가 부모노드
+# dq : BFS를 위한 Queue 자료구조
+parent = [0] * (N + 1)
+min_degree = [N + 1] * (N + 1)
+dq = deque([])
 
+# 초기화 : node 1(=root)에서 시작
+parent[1] = 1
+min_degree[1] = 1
+for i in relations[1]:
+    deque.append(dq, (i, 1, 1))
 
-if __name__ == "__main__":
-    import sys
-    input = sys.stdin.readline
+# BFS
+while dq:
+    # n: 이번 노드 / distance : 저번 노드에서 넘어온 차수 / before : 저번 노드
+    n, distance, before = dq.popleft()
 
-    N = int(input())
-    # idx 0은 사용하지 않음
-    parent = [n for n in range(N+1)]
-    # print(parent)
+    # 이번 차수가 최소차수라면
+    # n의 부모 노드는 before
+    if distance + 1 < min_degree[n]:
+        min_degree[n] = distance + 1
+        parent[n] = before
 
-    for _ in range(N-1):
-        u, v = map(int, input().split())
-        union_parent(u, v, parent)
-        # print(parent)
+    # n 와 연결된 노드 중 방문하지 않은 노드는 dq에 추가
+    for i in relations[n]:
+        if not parent[i]:
+            dq.append((i, distance + 1, n))
 
-    for i in range(2, N+1):
-        print(parent[i])
+# 정답 출력
+for i in range(2, N + 1):
+    print(parent[i])
