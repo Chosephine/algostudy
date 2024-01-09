6e268bba683309d40b99a2714f5a23bbfde3e461
Eunice <backji5@gmail.com>
2024-01-09 14:31:49 +0900

    modify : python boj 이상한 곱셈
    - Time: 44ms, Memory: 31120KB (Python3)
    - 타 풀이 대비 시간이 오래걸려서 다른 방법을 찾아봤더니 분배법칙을 적용하는 거였다. 이 문제는 구현이자 수학문제였던것..ㅎㅎ

--- python/implementation/boj1225.py
+++ python/implementation/boj1225.py
@@ -18,13 +18,24 @@ if __name__ == "__main__":
 
     A, B = input().split()
 
+    # ver.1 순회하면서 직접 곱셈
+
+    # answer = 0
+    #
+    # A = string_to_int(A)
+    # B = string_to_int(B)
+    #
+    #
+    # for a in A:
+    #     for b in B:
+    #         answer += a * b
+
+    # ver.2 분배법칙 활용
+    # 123 x 45 일 때, 1*4 + 1*5 + 2*4 + 2*5 + 3*4 + 3*5 = (1+2+3)*(4+5) 와 동일함
+
     A = string_to_int(A)
     B = string_to_int(B)
 
-    answer = 0
-
-    for a in A:
-        for b in B:
-            answer += a * b
+    answer = sum(A) * sum(B)
 
     print(answer)
