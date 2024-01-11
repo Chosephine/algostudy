7253a48317da58df62c4e05fbb97323d96b49818
Chosephine <backji5@gmail.com>
2024-01-11 15:54:17 +0900

    modify : python boj 트럭
    - Time: 384ms, Memory: 31120KB
    - 트럭 대수가 최대 1000대, 다리 길이가 최대 100이므로 완전 시뮬레이션이 가능할 것으로 예상함

--- python/implementation/boj13335.py
+++ python/implementation/boj13335.py
@@ -1,36 +1,44 @@
 # 백준 13335 트럭
 # https://www.acmicpc.net/problem/13335
 
-def grouping(arr: list, L: int) -> list:
-
-    len_of_arr = len(arr)
-    partition_idx = [0]
-
-    group_weight = 0
-    for i in range(len_of_arr):
-        if group_weight + arr[i] <= L:
-            group_weight += arr[i]
-        else:
-            group_weight = arr[i]
-            partition_idx.append(i)
-
-    partition_idx.append(len_of_arr)
-
-    return partition_idx
-
 if __name__ == "__main__":
     import sys
 
     input = sys.stdin.readline
 
+    # n: 트럭 대수, w: 다리 길이, L: 다리 하중
+    # trucks: 개별 트럭의 무게
     n, w, L = map(int, input().split())
     trucks = list(map(int, input().split()))
 
-    partition = grouping(trucks, L)
-    len_partition = len(partition)
+    # used_truck: 진입한 트럭 표시
+    # bridge: 다리 위의 트럭
+    used_truck = [0] * n
+    bridge = [0] * w
 
-    answer = -1 * (len_partition - 2)
-    for i in range(1, len_partition):
-        answer += w + partition[i] - partition[i-1]
+    # answer: 출력할 정답
+    # waiting_truck_idx: 다리 앞에서 대기 중인 트럭의 idx
+    answer = 0
+    waiting_truck_idx = 0
+    while True:
+        # weight_of_bridge: 다리 위 하중
+        weight_of_bridge = sum(bridge)
+
+        # 종료조건: 모든 트럭이 진입했고, 다리 위에 아무 트럭도 없을 때
+        if used_truck[n-1] and not weight_of_bridge:
+            break
+
+        # 1초가 흐르고, 트럭들이 이동함
+        answer += 1
+        weight_of_bridge -= bridge[w - 1]
+        for i in range(w-2, -1, -1):
+            bridge[i+1] = bridge[i]
+        bridge[0] = 0
+
+        # 대기 중인 트럭이 올라와도 다리 위 하중이 버틸 수 있다면 진입
+        if waiting_truck_idx < n and weight_of_bridge + trucks[waiting_truck_idx] <= L:
+            bridge[0] += trucks[waiting_truck_idx]
+            used_truck[waiting_truck_idx] = 1
+            waiting_truck_idx += 1
 
     print(answer)
