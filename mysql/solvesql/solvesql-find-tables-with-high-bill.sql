467809bb5f36f0ab61df94b5bf5e0bb99c037d54
Chosephine <backji5@gmail.com>
2024-01-13 18:05:08 +0900

    modify : mysql solvesql 많이 주문한 테이블 찾기
    - 변수 선언 활용해 봄

--- mysql/solvesql/solvesql-find-tables-with-high-bill.sql
+++ mysql/solvesql/solvesql-find-tables-with-high-bill.sql
@@ -1,6 +1,18 @@
 -- solvesql 많이 주문한 테이블 찾기
 -- https://solvesql.com/problems/find-tables-with-high-bill/
 
+-- ver.sqlite
 SELECT *
 FROM tips
 WHERE total_bill > (SELECT AVG(total_bill) FROM tips);
+
+-- ver.mysql : 변수선언 활용
+-- 이게 문제 풀때도 적용되는지는 확인이 필요한 것 같다. 다음에 mysql 쿼리 문제 풀 대 한 번 해보기!
+
+-- 변수 선언
+SET @avg_total_bill = (SELECT AVG(total_bill) FROM tips);
+
+-- 본쿼리
+SELECT *
+FROM tips
+WHERE total_bill > @avg_total_bill;
