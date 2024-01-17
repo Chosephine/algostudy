aa155a2da04a4a5c3384060bfb6e110c602f2cd3
Eunice <backji5@gmail.com>
2024-01-17 17:54:07 +0900

    modiify : python boj 계단 오르기
    - Time: 40ms, Memory: 31120KB
    - ver3. 점화식 활용(구글링ㅠ)

--- python/boj2579.py
+++ python/boj2579.py
@@ -4,28 +4,17 @@
 def max_score(n: int, score_list: list) -> int:
     # 사용할 자료구조
     score_list = [0] + score_list
-    cumulated_score = [[] for _ in range(n + 1)]
+    dp = [0] * (n + 1)
 
-    # 초기화
-    cumulated_score[0].append((score_list[0], 1))
+    for i in range(n + 1):
+        if i < 2:
+            dp[i] = score_list[i]
+        elif i < 3:
+            dp[i] = max(score_list[i - 1] + score_list[i], dp[i-2] + score_list[i])
+        else:
+            dp[i] = max(dp[i-3] + score_list[i - 1] + score_list[i], dp[i-2] + score_list[i])
 
-    for i in range(n):
-        for score_tuple in cumulated_score[i]:
-            score, history = score_tuple
-
-            if history >= 2 and i != 1:
-                if i + 2 < n + 1 :
-                    # 한칸 이동 불가
-                    cumulated_score[i + 2].append((score + score_list[i + 2], 1))
-
-            else:
-                # 한칸 이동 및 두칸 이동 모두 가능
-                if i + 1 < n + 1:
-                    cumulated_score[i + 1].append((score + score_list[i + 1], history + 1))
-                if i + 2 < n + 1:
-                    cumulated_score[i + 2].append((score + score_list[i + 2], 1))
-
-    return max(cumulated_score[-1])[0]
+    return dp[-1]
 
 
 if __name__ == "__main__":
