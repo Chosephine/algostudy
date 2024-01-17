3ac8b4fbc4e2ee1960657804bfe4e5dd3fe0850b
Eunice <backji5@gmail.com>
2024-01-17 17:35:21 +0900

    modify : python boj 계단 오르기
    - 메모리 초과
    - ver2. "현재의 최적이 전체의 최적이 아닐 수 있다"

--- python/boj2579.py
+++ python/boj2579.py
@@ -4,34 +4,28 @@
 def max_score(n: int, score_list: list) -> int:
     # 사용할 자료구조
     score_list = [0] + score_list
-    cumulated_score = [0] * (n + 1)
-    histories = [0] * (n + 1)
+    cumulated_score = [[] for _ in range(n + 1)]
 
     # 초기화
-    cumulated_score[0] = score_list[0]
-    histories[0] = 1
+    cumulated_score[0].append((score_list[0], 1))
 
     for i in range(n):
-        # 이번 계단에서 다음 계단으로 넘어갈 때 계산
+        for score_tuple in cumulated_score[i]:
+            score, history = score_tuple
 
-        # 두칸 건너 뛸 땐 histories 1로 설정
-        # 한칸 건너 뛸 땐 histories 에 1 추가
-        if histories[i] >= 2 and i != 1:
-            if i + 2 < n + 1 and cumulated_score[i + 2] < cumulated_score[i] + score_list[i + 2]:
-                # 한칸 이동 불가
-                cumulated_score[i + 2] = cumulated_score[i] + score_list[i + 2]
-                histories[i + 2] = 1
+            if history >= 2 and i != 1:
+                if i + 2 < n + 1 :
+                    # 한칸 이동 불가
+                    cumulated_score[i + 2].append((score + score_list[i + 2], 1))
 
-        else:
-            # 한칸 이동 및 두칸 이동 모두 가능
-            if i + 1 < n + 1 and cumulated_score[i + 1] < cumulated_score[i] + score_list[i + 1]:
-                cumulated_score[i + 1] = cumulated_score[i] + score_list[i + 1]
-                histories[i + 1] = histories[i] + 1
-            if i + 2 < n + 1 and cumulated_score[i + 2] < cumulated_score[i] + score_list[i + 2]:
-                cumulated_score[i + 2] = cumulated_score[i] + score_list[i + 2]
-                histories[i + 2] = 1
+            else:
+                # 한칸 이동 및 두칸 이동 모두 가능
+                if i + 1 < n + 1:
+                    cumulated_score[i + 1].append((score + score_list[i + 1], history + 1))
+                if i + 2 < n + 1:
+                    cumulated_score[i + 2].append((score + score_list[i + 2], 1))
 
-    return cumulated_score[-1]
+    return max(cumulated_score[-1])[0]
 
 
 if __name__ == "__main__":
