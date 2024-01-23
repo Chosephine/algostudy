1b0d8f8fa0fd99945a1e5c90d1e84624216d888f
Eunice <backji5@gmail.com>
2024-01-23 19:17:10 +0900

    modify : python boj 2xn 타일링
    - Time: 44ms, Memory: 31252KB
    - 함수를 통한 관심사 분리
    - 효율 개선은 이루어지지 않았다ㅠ

--- python/dynamic_programming/boj11726.py
+++ python/dynamic_programming/boj11726.py
@@ -1,19 +1,25 @@
 # 백준 11726 2×n 타일링
 # https://www.acmicpc.net/problem/11726
 
+def solution(n: int)->int:
+    dp = [0] * (n + 1)
+
+    if n <= 2:
+        return n
+
+    if n > 2:
+        dp[1] = 1
+        dp[2] = 2
+
+        for i in range(3, n + 1):
+            dp[i] = dp[i - 1] + dp[i - 2]
+
+    return dp[n] % 10007
+
 if __name__ == "__main__":
     import sys
     input = sys.stdin.readline
 
     N = int(input())
 
-    dp = [0] * (N + 1)
-    dp[1] = 1
-
-    if N > 1:
-        dp[2] = 2
-
-        for i in range(3, N + 1):
-            dp[i] = dp[i - 1] + dp[i - 2]
-
-    print(dp[N] % 10007)
+    print(solution(N))
