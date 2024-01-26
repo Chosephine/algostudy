194ca50d6ae4ec55fb0881954feb84e9ff4b841f
Chosephine <backji5@gmail.com>
2024-01-26 15:12:51 +0900

    add : python boj 아스키코드
    - Time: 40ms, Memory: 31120KB

---
+++ python/basic/boj11654.py
@@ -0,0 +1,10 @@
+# 백준 11654 아스키코드
+# https://www.acmicpc.net/problem/11654
+
+if __name__ == '__main__':
+    import sys
+    input = sys.stdin.readline
+
+    X = input().rstrip()
+
+    print(ord(X))
