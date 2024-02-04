0118b6e986d2b654067c50e845f808bb36db5499
Eunice <backji5@gmail.com>
2024-02-04 15:40:34 +0900

    modify : java boj 소수 구하기
    - Time: 244ms, Memory: 20248KB
    - 에라토스의 체 알고리즘 활용

--- java/src/solve064.java
+++ java/src/solve064.java
@@ -8,19 +8,34 @@ import java.util.StringTokenizer;
  * https://www.acmicpc.net/problem/1929
  */
 
+
 public class solve064 {
 	// ver.1 숫자 개별적으로 소수판별 : 시간초과 
-	public static boolean isPrime(int number) {
-		boolean answer = true;
+//	public static boolean isPrime(int number) {
+//		boolean answer = true;
+//		
+//		for (int i = number - 1; i > 1; i--) {
+//			if (number % i == 0) {
+//				answer = false;
+//				break;
+//			}
+//		}
+//			
+//		return answer;
+//	}
+	
+	// ver.2 소수 판별을 위한 빠른 알고리즘인 "에라토스의 체" 사용한 알고리
+	public static void eratos(boolean[] sieve) {
+		sieve[0] = sieve[1] = false;
 		
-		for (int i = number - 1; i > 1; i--) {
-			if (number % i == 0) {
-				answer = false;
-				break;
-			}
+		for (int i = 0; i < sieve.length; i++) {
+			if (sieve[i] != false) {
+				sieve[i] = true;
+				for (int j = 2; i * j < sieve.length; j++) {
+					sieve[i*j] = false;
+				}
+			} 
 		}
-			
-		return answer;
 	}
 	
 	public static void main(String[] args) throws Exception {
@@ -32,15 +47,27 @@ public class solve064 {
 		int end = Integer.parseInt(st.nextToken());
 		
 		// ver.1 start~end 순회하며 개별 숫자 소수 여부 파악 : 시간초과 
-		for (int n = start; n < end + 1; n++) {
-			
-			if (isPrime(n)) {
-			 	
-				sb.append(n);
-			 	sb.append('\n');
-			 	
+//		for (int n = start; n < end + 1; n++) {
+//			
+//			if (isPrime(n)) {
+//			 	
+//				sb.append(n);
+//			 	sb.append('\n');
+//			 	
+//			}
+//			
+//		}
+		
+		// ver.2 에라토스의 체 활용한 알고리즘 
+		boolean[] isPrime = new boolean[end + 1];
+		Arrays.fill(isPrime, true);
+		eratos(isPrime);
+		
+		for (int i = start; i <= end; i++) {
+			if (isPrime[i]) {
+				sb.append(i);
+				sb.append('\n');
 			}
-			
 		}
 		
 		System.out.println(sb);
