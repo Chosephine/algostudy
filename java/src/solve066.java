467c09bfc3794218a783e34cedc66d5b089d5d09
Chosephine <backji5@gmail.com>
2024-02-07 13:04:53 +0900

    modify : java boj 최대공약수와 최소공배수
    - Time: 120ms, Memory: 14232KB

--- java/src/solve066.java
+++ java/src/solve066.java
@@ -10,6 +10,7 @@ import java.util.StringTokenizer;
 
 public class solve066 {
 	
+	// ver.1 직접 계산을 위한 매서드
 	// 소수 판별을 위한 에라토스의 체 변형해서 사용
 	// 1은 소수는 아니지만 약수는 될 수 있으므로 true
 	public static void eratos(boolean[] arr) {
@@ -30,6 +31,24 @@ public class solve066 {
 		
 	}
 	
+	// ver.2 유클리드 호제법 사용 매서드
+	public static int euclid(int x, int y) {
+		
+		if (x == 0) {
+			
+			return y;
+			
+		} else if (y == 0) {
+			
+			return x;
+			
+		} else {
+			
+			return euclid(y, x % y);
+			
+		}
+	}
+	
 	public static void main(String[] args) throws Exception{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
@@ -37,32 +56,41 @@ public class solve066 {
 		int n1 = Integer.parseInt(st.nextToken());
 		int n2 = Integer.parseInt(st.nextToken());
 		
+		
+		// ver.1 직접 계산
 		// 소수 판별을 위한 prime 배열 생성
-		int min = Integer.min(n1, n2);
-		boolean[] prime = new boolean[min + 1];
-		Arrays.fill(prime, true);
-		eratos(prime);
+//		int min = Integer.min(n1, n2);
+//		boolean[] prime = new boolean[min + 1];
+//		Arrays.fill(prime, true);
+//		eratos(prime);
+//		
+//		// 최대공악수 계산
+//		int gcd = 1;
+//		for (int i = 2; i < min + 1; i++) {
+//			
+//			while (true) {
+//				
+//				if (prime[i] & n1 % i == 0 & n2 % i == 0) {
+//					
+//					gcd *= i;
+//					n1 /= i;
+//					n2 /= i;
+//					
+//				}
+//				else break;
+//			}
+//			
+//		}
+//		
+//		
+//		System.out.println(gcd);
+//		System.out.println(gcd * n1 * n2);
 		
-		// 최대공악수 계산
-		int gcd = 1;
-		for (int i = 2; i < min + 1; i++) {
-			
-			while (true) {
-				
-				if (prime[i] & n1 % i == 0 & n2 % i == 0) {
-					
-					gcd *= i;
-					n1 /= i;
-					n2 /= i;
-					
-				}
-				else break;
-			}
-			
-		}
-		
+		// ver.2 유클리드 호제법 사용
+		int gcd = euclid(Integer.max(n1, n2), Integer.min(n1, n2));
+
 		System.out.println(gcd);
-		System.out.println(gcd * n1 * n2);
+		System.out.println(n1 * n2 / gcd);
 		
 	}
 
