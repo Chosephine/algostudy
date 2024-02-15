cccb6acf67ad65723feef1c16f828e3b2382f63b
Eunice <backji5@gmail.com>
2024-02-15 22:45:44 +0900

    modify : java boj 스
    - Time: 232ms, Memory: 20024KB
    - ArrayList 대신 int 배열을 선언하여 사용하니 시간초과를 해결할 수 있었다!

--- java/src/solve072.java
+++ java/src/solve072.java
@@ -9,44 +9,37 @@ import java.util.StringTokenizer;
  * */
 
 public class solve072 {
-	private static ArrayList<Integer> customStack = new ArrayList<Integer>();
 	private static int pointer = 0;
 	
-	public static void push(int n) {
-		try {
-			customStack.get(pointer);
-			customStack.set(pointer, n);
-			pointer += 1;
-		} catch (Exception IndexOutOfBoundsException) {
-			customStack.add(n);
-			pointer += 1;
-
-		}
+	public static void push(int[] customStack, int n) {
+		
+		customStack[pointer] = n;
+		pointer += 1;
 		
 	}
-	public static int pop() {
+	public static int pop(int[] customStack) {
 		pointer -= 1;
 		if (pointer >= 0) {
-			return customStack.get(pointer);
+			return customStack[pointer];
 		} else {
 			pointer += 1;
 			return -1;
 		}
 		
 	}
-	public static int size() {
+	public static int size(int[] customStack) {
 		return pointer;
 	}
-	public static int empty() {
+	public static int empty(int[] customStack) {
 		if (pointer > 0) {
 			return 0;
 		} else {
 			return 1;
 		}
 	}
-	public static int top() {
+	public static int top(int[] customStack) {
 		if (pointer > 0) {
-			return customStack.get(pointer - 1);
+			return customStack[pointer - 1];
 		} else {
 			return -1;
 		}
@@ -58,20 +51,24 @@ public class solve072 {
 		
 		int cmdCnt = Integer.parseInt(br.readLine());
 		
+		int[] customStack = new int[cmdCnt];
+		
+		
 		for(int i = 0; i < cmdCnt ; i++) {
 			StringTokenizer st = new StringTokenizer(br.readLine());
 			String cmd = st.nextToken();
 			
 			switch (cmd) {
-				case "push" : push(Integer.parseInt(st.nextToken()));
+				case "push" : push(customStack, Integer.parseInt(st.nextToken()));
 					break;
-				case "pop" : sb.append(pop() + "\n");
+				case "pop" : sb.append(pop(customStack) + "\n");
+
 					break;
-				case "size" : sb.append(size() + "\n");
+				case "size" : sb.append(size(customStack) + "\n");
 					break;
-				case "empty" : sb.append(empty() + "\n");
+				case "empty" : sb.append(empty(customStack) + "\n");
 					break;
-				case "top" : sb.append(top() + "\n");
+				case "top" : sb.append(top(customStack) + "\n");
 					break;
 			}
 			
